5292cb21419dce77092ae13c2e45e8005a4e6040
hanadoggy <hanayor6346@gmail.com>
2025-05-16 13:00:11 +0900

    refactor: upgrade dynamic path segments
    change dynamic path segments to handle number

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
+
+monthly_chalenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 mintues every day!",
+    "march": "Learn Django for at least 30 mintues every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 mintues every day!",
+    "june": "Learn Django for at least 30 mintues every day!",
+    "july": "Eat no meat for the entire month!",
+    "auguest": "Walk for at least 20 mintues every day!",
+    "september": "Learn Django for at least 30 mintues every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 mintues every day!",
+    "december": "Learn Django for at least 30 mintues every day!"
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_chalenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-
-    if month == 'january':
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == 'february':
-        challenge_text = "Walk for at least 20 mintues every day!"
-    elif month == 'march':
-        challenge_text = "Learn Django for at least 30 mintues every day!"
-    else:
+    try:
+        challenge_text = monthly_chalenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
