2a7732c11552f21251b63e9c0519ac7028b898fa
hanadoggy <hanayor6346@gmail.com>
2025-05-16 11:22:54 +0900

    feat: add challenges pages
    add challenges pages for january, february, march

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 mintues every day!")
+
+def march(request):
+    return HttpResponse("Learn Django for at least 30 mintues every day!")
