b1597edae7ee5f646425c12ba2c7808962e00b03
hanadoggy <hanayor6346@gmail.com>
2025-05-16 11:31:10 +0900

    refactor: change to dynamic path segment
    change urls and views to use dynamic path segment

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
+def monthly_challenge(request, month):
+    challenge_text = None
 
-def february(request):
-    return HttpResponse("Walk for at least 20 mintues every day!")
-
-def march(request):
-    return HttpResponse("Learn Django for at least 30 mintues every day!")
+    if month == 'january':
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == 'february':
+        challenge_text = "Walk for at least 20 mintues every day!"
+    elif month == 'march':
+        challenge_text = "Learn Django for at least 30 mintues every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
