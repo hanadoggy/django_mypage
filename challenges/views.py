1044ea7de02364b16002ba5c9fadd989f1d62b01
hanadoggy <hanayor6346@gmail.com>
2025-05-16 16:23:53 +0900

    feat: add index page
    add index page and fix response message

--- challenges/views.py
+++ challenges/views.py
@@ -18,6 +18,17 @@ monthly_chalenges = {
     "december": "Learn Django for at least 30 mintues every day!"
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_chalenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = F"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_chalenges.keys())
     if month > len(months):
@@ -30,6 +41,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_chalenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
